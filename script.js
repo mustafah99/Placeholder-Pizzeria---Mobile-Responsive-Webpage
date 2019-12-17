ba9779ded96a473154d964c605f4ddfd99b4a1cf
Musse <mustafasaidhelal@gmail.com>
2019-12-17 02:49:38 +0100

    updates

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function initMap() {
     const helsingborg = {lat: 56.049210, lng: 12.689240};
     
     // Centering the map on the location.
-    const map = new google.maps.Map(
-        document.getElementById('googleMaps'), {zoom: 5, center: helsingborg});
+    let map = new google.maps.Map(
+        document.getElementById('googleMaps'), {zoom: 15, center: helsingborg});
 
     // Setting a marker centered on the location we centered our map on.
     const mapMarker = new google.maps.Marker({position: helsingborg, map: map});
